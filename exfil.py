ca0d51ba29216ab6e4a0260fb6eb42f804264928
Stephen Haywood <stephen@averagesecurityguy.info>
2014-09-29 22:51:00 -0400

    Remove requirement for data. Check for data when sending.

--- exfil.py
+++ exfil.py
@@ -9,7 +9,7 @@ import importlib
 #
 p = argparse.ArgumentParser(description='Exfiltrate data.')
 
-data = p.add_mutually_exclusive_group(required=True)
+data = p.add_mutually_exclusive_group()
 data.add_argument('-d', metavar='string', help='String of data to exfiltrate.')
 data.add_argument('-f', metavar='filename', help='File to send.')
 
@@ -30,7 +30,7 @@ try:
     m = importlib.import_module('lib.' + args.module_name)
 
 except:
-    print('Error: module {0} was not found.'.format(args.module_name))
+    print('Error: Module {0} was not found.'.format(args.module_name))
 
 
 #
@@ -39,6 +39,9 @@ except:
 if args.l is True:
     m.listen(args.p)
 else:
+    if (args.d is None) and (args.f is None):
+        print('Error: No data to send.')
+
     if args.d is not None:
         data = args.d
 
