ff7239b915093c6915d05c15362c5e86341bd6cb
Stephen Haywood <stephen@averagesecurityguy.info>
2014-10-01 02:01:08 -0400

    Add port to logging. Fix print statement.

--- lib/dns_lookup.py
+++ lib/dns_lookup.py
@@ -8,7 +8,7 @@ def listen(port):
     if port is None:
         port = PORT
 
-    print('Listening for data via DNS.')
+    print('Listening for data via DNS on port {0}.'.format(port))
     l = network.get_listener('UDP', port)
 
     print('Data Received:')
@@ -25,12 +25,12 @@ def send(server, port, data):
     if port is None:
         port = PORT
 
-    print('Sending data to {0} via DNS.'.format(server))
+    print('Sending data via DNS to {0} on port {1}.'.format(server, port))
 
     print('Data Sent:')
     for n in range(0, len(data), BLOCK_SIZE):
         block = data[n:n + BLOCK_SIZE]
-        print block
+        print(block)
 
         enc = network.encode_data(block)
         network.send_dns_query(server, port, enc)
