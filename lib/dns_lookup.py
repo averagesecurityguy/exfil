669530572ea286eb681d8eaeff7c0956e5fa8bd1
averagesecurityguy <stephen@averagesecurityguy.info>
2017-04-16 22:54:56 -0400

    Buld proper query name.

--- lib/dns_lookup.py
+++ lib/dns_lookup.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import network
 
-# The max length of a DNS label is 253. Since we are base64 encoding our data
-# the longest block we can use is 185 bytes.
-BLOCK_SIZE = 185
+# The DNS query has to be in the format sub.domain.tld and each part can be a
+# max of 63 characters. Since we are Base64 encoding the data the actual number
+# of bytes we can send in each part is 45. We can send a max of 4 parts, which
+# is a total of 180 characters.
+BLOCK_SIZE = 180
 PORT = 53
 
 def listen(port):
@@ -18,8 +20,10 @@ def listen(port):
         data, addr = l.recvfrom(1500)
 
         dns = network.parse_dns(data)
-        name = str(dns.get_q().get_qname()) 
+        name = str(dns.get_q().get_qname())
+        name = name.replace('.', '')
         dec = network.decode_data(name.rstrip('.'))
+
         print(dec)
 
 
@@ -30,9 +34,11 @@ def send(server, port, data):
     print('Sending data via DNS to {0} on port {1}.'.format(server, port))
 
     print('Data Sent:')
+
     for n in range(0, len(data), BLOCK_SIZE):
         block = data[n:n + BLOCK_SIZE]
         print(block)
 
         enc = network.encode_data(block)
+        enc = enc[:63] + '.' + enc[63:126] + '.' + enc[126:189] + '.' + enc[189:]
         network.send_dns_query(server, port, enc)
